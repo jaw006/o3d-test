34e631f9be2194faf19604f7070c24041cd58cbf
jaw006 <jaw006@ucsd.edu>
2020-09-08 07:08:13 -0700

    add RGBDCaptureSet class

---
+++ RGBDCaptureSet.cpp
@@ -0,0 +1,1 @@
+#include "RGBDCaptureSet.h"
---
+++ RGBDCaptureSet.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Reco3D
+{
+    // Manages a set of captures for use in reconstruciton
+    class RGBDCaptureSet
+    {
+
+    };
+}
