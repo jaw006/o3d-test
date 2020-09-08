2a747895bf024b9f53644aef92929063a1030cd9
jaw006 <jaw006@ucsd.edu>
2020-09-08 07:39:52 -0700

    add skeleton of RGBDCaptureSet class

--- RGBDCaptureSet.cpp
+++ RGBDCaptureSet.cpp
@@ -1,1 +1,13 @@
 #include "RGBDCaptureSet.h"
+
+Reco3D::RGBDCaptureSet::RGBDCaptureSet()
+{
+}
+
+Reco3D::RGBDCaptureSet::~RGBDCaptureSet()
+{
+}
+
+void Reco3D::RGBDCaptureSet::AddCapture(std::shared_ptr<RGBDCapture_t> capture)
+{
+}
--- RGBDCaptureSet.h
+++ RGBDCaptureSet.h
@@ -1,10 +1,30 @@
 #pragma once
+#include <vector>
+#include "PointCloud.h"
+#include "PointsToMesh.h"
+#include "RGBDToPoints.h"
+#include "Types.h"
 
 namespace Reco3D
 {
-    // Manages a set of captures for use in reconstruciton
+    // Takes a capture, converts it to points
+    // and stores the point cloud representations
     class RGBDCaptureSet
     {
+    private:
+        std::vector<PointCloud>    points_;
+        std::vector<RGBDCapture_t> captures_;
 
+        std::shared_ptr<RGBDToPoints>   rgbdToPoints_;
+        std::shared_ptr<PointsToMesh>   pointsToMesh_;
+        std::shared_ptr<o3d_PointCloud> combinedPoints_;
+        std::shared_ptr<o3d_TriMesh>    combinedMesh_;
+
+    public:
+        RGBDCaptureSet();
+        ~RGBDCaptureSet();
+        void AddCapture(std::shared_ptr<RGBDCapture_t> capture);
+        std::shared_ptr<o3d_PointCloud> GetCombinedPointCloud();
+        std::shared_ptr<o3d_TriMesh> GetCombinedTriangleMesh();
     };
 }
