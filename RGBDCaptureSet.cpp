5ce34ba5ffaf999d24f7a1c08c42520a1954fd09
jaw006 <jaw006@ucsd.edu>
2020-09-08 11:34:10 -0700

    add PointsVector to RGBDCaptureSet

--- RGBDCaptureSet.cpp
+++ RGBDCaptureSet.cpp
@@ -4,7 +4,8 @@ Reco3D::RGBDCaptureSet::RGBDCaptureSet() :
     rgbdToPoints_  (std::shared_ptr<RGBDToPoints>   (new Reco3D::RGBDToPoints())),
     pointsToMesh_  (std::shared_ptr<PointsToMesh>   (new Reco3D::PointsToMesh())),
     combinedPoints_(std::shared_ptr<o3d_PointCloud> (new Reco3D::o3d_PointCloud())),
-    combinedMesh_  (std::shared_ptr<o3d_TriMesh>    (new Reco3D::o3d_TriMesh()))
+    combinedMesh_  (std::shared_ptr<o3d_TriMesh>    (new Reco3D::o3d_TriMesh())),
+    pointsVector_  (std::shared_ptr<PointsVector>   (new Reco3D::PointsVector()))
 {
 }
 
@@ -15,18 +16,28 @@ Reco3D::RGBDCaptureSet::~RGBDCaptureSet()
 void Reco3D::RGBDCaptureSet::AddCapture(std::shared_ptr<RGBDCapture_t> capture)
 {
     // TODO: Check duplicates?
-    if (capture != nullptr)
+    if (capture == nullptr)
     {
-        captures_.push_back(*capture);
+        return;
     }
+    // Insert capture
+    captures_.push_back(*capture);
+//    // Add created point cloud
+    std::shared_ptr<Reco3D::PointCloud> newPoints(rgbdToPoints_->ToPointCloud(capture));
+    pointsVector_->AddPoints(newPoints);
 }
 
-std::shared_ptr<Reco3D::o3d_PointCloud> Reco3D::RGBDCaptureSet::GetCombinedPointCloud()
+size_t Reco3D::RGBDCaptureSet::Count()
 {
-    return std::shared_ptr<o3d_PointCloud>();
+    return captures_.size();
+}
+
+std::shared_ptr<Reco3D::PointCloud> Reco3D::RGBDCaptureSet::GetCombinedPointCloud()
+{
+    return pointsVector_->GetCombinedPoints();
 }
 
 std::shared_ptr<Reco3D::o3d_TriMesh> Reco3D::RGBDCaptureSet::GetCombinedTriangleMesh()
 {
-    return std::shared_ptr<o3d_TriMesh>();
+    return pointsToMesh_->ToMesh(pointsVector_);
 }
--- RGBDCaptureSet.h
+++ RGBDCaptureSet.h
@@ -13,18 +13,22 @@ namespace Reco3D
     {
     private:
         std::vector<RGBDCapture_t> captures_;
-        std::vector<PointCloud>    points_;
+        std::vector<std::shared_ptr<PointCloud>>    points_;
 
         std::shared_ptr<RGBDToPoints>   rgbdToPoints_;
         std::shared_ptr<PointsToMesh>   pointsToMesh_;
+
         std::shared_ptr<o3d_PointCloud> combinedPoints_;
         std::shared_ptr<o3d_TriMesh>    combinedMesh_;
+
+        std::shared_ptr<PointsVector>   pointsVector_;
 
     public:
         RGBDCaptureSet();
         ~RGBDCaptureSet();
         void AddCapture(std::shared_ptr<RGBDCapture_t> capture);
-        std::shared_ptr<o3d_PointCloud> GetCombinedPointCloud();
+        size_t Count();
+        std::shared_ptr<PointCloud> GetCombinedPointCloud();
         std::shared_ptr<o3d_TriMesh> GetCombinedTriangleMesh();
     };
 }
