f4bbd54ffe1f5a0cc88fd4cf7d2afa2bd85a284e
jaw006 <jaw006@ucsd.edu>
2020-09-15 02:29:07 -0700

    Simplify matrix transforms

--- PointsToMesh.cpp
+++ PointsToMesh.cpp
@@ -70,6 +70,11 @@ std::shared_ptr<Reco3D::o3d_TriMesh> Reco3D::PointsToMesh::ToMesh(std::shared_pt
    std::cout << "Decimating " << numTriangles << " into " << numTrianglesDecimated << " triangles." << std::endl;
    output->SimplifyQuadricDecimation((int)numTrianglesDecimated);
 
+    if (!output->HasVertexNormals() || !output->HasTriangleNormals())
+    {
+        output = std::make_shared<o3d_TriMesh>(output->ComputeVertexNormals().ComputeTriangleNormals());
+    }
+
    // Color Mapping
 
    // Post processing
@@ -128,14 +133,26 @@ bool Reco3D::PointsVector::AddPoints(std::shared_ptr<Reco3D::PointCloud> points)
     ImagePose pose = points->GetPose();
     ImageQuaternion quat = points->GetQuat();
     Eigen::Matrix3d quatRotation = open3d::geometry::Geometry3D::GetRotationMatrixFromQuaternion(quat);
+    Eigen::Matrix4d quatRotationMtx = Eigen::Matrix4d::Identity();
+    quatRotationMtx.topLeftCorner(3, 3) = quatRotation;
+
     Eigen::Vector3d posePosition = { pose(0,3), pose(1,3), pose(2,3) };
     ImagePose posePositionMatrix = ExtractPoseTransform(pose);
+    ImagePose inversePosePositionMatrix = posePositionMatrix;
+    inversePosePositionMatrix(0, 3) = -posePositionMatrix(0,3);
+    inversePosePositionMatrix(1, 3) = -posePositionMatrix(1,3);
+    inversePosePositionMatrix(2, 3) = -posePositionMatrix(2,3);
 
     // Transform points by rotating 180 on Z axis and -90 on Y axis
     const Eigen::Affine3d aff = Eigen::Affine3d::Identity() * Eigen::AngleAxisd(-M_PI/2.0, Eigen::Vector3d::UnitX());
-    points->GetPoints()->Transform(aff.matrix());
-    points->GetPoints()->Transform(posePositionMatrix);
-    points->GetPoints()->Rotate(quatRotation, posePosition);
+//    Eigen::Matrix4d mtx = Eigen::Matrix4d::Identity() * aff.matrix() * posePositionMatrix * inversePosePositionMatrix * quatRotationMtx * posePositionMatrix;
+    Eigen::Matrix4d mtx = posePositionMatrix * quatRotationMtx * aff.matrix();
+    std::cout << "Pose: " << pose << std::endl;
+    std::cout << "Tform Mtx: " << mtx << std::endl;
+//    points->GetPoints()->Transform(aff.matrix());
+//    points->GetPoints()->Transform(posePositionMatrix);
+//    points->GetPoints()->Rotate(quatRotation, posePosition);
+    points->GetPoints()->Transform(mtx);
 
     // Registration
     // Not sure if points get transformed when passed
@@ -144,17 +161,20 @@ bool Reco3D::PointsVector::AddPoints(std::shared_ptr<Reco3D::PointCloud> points)
     }
     else
     {
-        Eigen::Matrix4d m = Eigen::Matrix4d::Identity();
+        Eigen::Matrix4d& m = mtx;
         double max_correspondence_distance = 10.0;
-        auto current_reg_result = open3d::registration::EvaluateRegistration(*(pointsVector_.back())->GetPoints(), *points->GetPoints(), max_correspondence_distance, m);
+        auto current_reg_result = open3d::registration::EvaluateRegistration(*(pointsVector_.back())->GetPoints(), *points->GetPoints(), max_correspondence_distance, mtx);
         std::cout << "Current fitness:" << current_reg_result.fitness_ << std::endl;
         std::cout << "Current RMSE:" << current_reg_result.inlier_rmse_<< std::endl;
-        auto reg_result = RegisterPoints(pointsVector_.back(), points, m);
+        std::cout << "Current transformation:" << Eigen::Matrix4d::Identity() * aff.matrix() * inversePosePositionMatrix * quatRotationMtx * posePositionMatrix << std::endl;
+
 
         // Transform points based on registration results
+        auto reg_result = RegisterPoints(pointsVector_.back(), points, m);
         Eigen::Matrix4d reg_transform = reg_result.transformation_;
         auto regTranslation = ExtractPoseTransform(Eigen::Matrix4d(reg_transform));
         Eigen::Vector3d translationVector{ regTranslation(0,3),regTranslation(1,3), regTranslation(2,3) };
+
 //        posePositionMatrix(0, 3) += reg_transform(0, 3);
 //        posePositionMatrix(1, 3) += reg_transform(1, 3);
 //        posePositionMatrix(2, 3) += reg_transform(2, 3);
