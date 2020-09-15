ca3568d8d10bbbc5f1ef03141995db9d4840b576
jaw006 <jaw006@ucsd.edu>
2020-09-14 22:57:13 -0700

    Define types for constructing camera poses

--- Types.h
+++ Types.h
@@ -6,6 +6,9 @@ namespace Reco3D
 {
 	typedef Eigen::Matrix4d ImagePose;
 	typedef Eigen::Vector4d ImageQuaternion;
+	typedef open3d::camera::PinholeCameraTrajectory CameraPose;
+	typedef open3d::camera::PinholeCameraParameters CameraPoseParameters;
+
 	typedef std::shared_ptr<open3d::geometry::RGBDImage> ImageRGBD;
 
 	// Struct to encapsulate pose transform and image data
