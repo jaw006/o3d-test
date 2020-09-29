b04c034e98d672cf6b5fb4f347c60d7b6463cc98
jaw006 <jaw006@ucsd.edu>
2020-09-28 21:09:25 -0700

    Add check for empty filename

--- kinectImage.sh
+++ kinectImage.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+# https://docs.microsoft.com/en-us/azure/kinect-dk/record-file-format
 FILENAME=$1
-KINECT_DIR="/c/Program Files/Azure Kinect SDK v1.2.0/tools"
-RECORDER="k4arecorder.exe"
-OPS="--device 0 -d NFOV_UNBINNED -c 720p -l 0.1 -e 0 /c/temp/${FILENAME}.mkv"
+TEMP_DIR="/c/temp/"
+RECORDER="/c/Program Files/Azure Kinect SDK v1.2.0/tools/k4arecorder.exe"
+FFMPEG="/d/ffmpeg/bin/"
+OPS="--device 0 -d NFOV_UNBINNED -c 720p -l 0.1 -e 0 ${TEMP_DIR}${FILENAME}.mkv"
+
+echo ${FILENAME}
+
+if [ "${FILENAME}" == "" ]; then
+	echo "usage: ./kinectImage.sh <FILENAME>"
+	exit
+fi
 
 # Record video
-"${KINECT_DIR}/${RECORDER}" ${OPS}
+"${RECORDER}" ${OPS}
+
+# Extract frames
