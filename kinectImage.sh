a95c0e544ca26b87a39f0adbf1f0e36a19826087
jaw006 <jaw006@ucsd.edu>
2020-09-28 21:02:22 -0700

    Script records video

--- kinectImage.sh
+++ kinectImage.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+FILENAME=$1
+KINECT_DIR="/c/Program Files/Azure Kinect SDK v1.2.0/tools"
+RECORDER="k4arecorder.exe"
+OPS="--device 0 -d NFOV_UNBINNED -c 720p -l 0.1 -e 0 /c/temp/${FILENAME}.mkv"
+
+# Record video
+"${KINECT_DIR}/${RECORDER}" ${OPS}
